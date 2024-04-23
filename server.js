02f1162c10fa4994261fcd225e7cb6767de5b293
Matheus <matheuscardhosoo@gmail.com>
2024-04-23 19:29:39 -0300

    Add some example routes

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ import http from "http";
 
 const PORT = 3000;
 
+const routes = {
+  "/": (req, res) => {
+    res.end("Welcome to the homepage!");
+  },
+  "/about": (req, res) => {
+    res.end("Welcome to the about page!");
+  },
+"/contact": (req, res) => {
+    res.end("Welcome to the contact page!");
+},
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World\n");
+  routes[req.url](req, res);
 });
 
 server.listen(PORT, () => {
