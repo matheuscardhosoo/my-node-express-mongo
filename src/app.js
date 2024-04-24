a88e8589f197eadf591497aa2dd23a6a65598fae
Matheus <matheuscardhosoo@gmail.com>
2024-04-23 21:23:39 -0300

    Create a mocked api for books objects

--- src/app.js
+++ src/app.js
@@ -1,17 +1,61 @@
 import express from "express";
 
+import books from "./books.js";
+
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.status(200).send("Welcome to the homepage!");
+app.get("/books", (req, res) => {
+    res.status(200).json(books);
 });
 
-app.get("/about", (req, res) => {
-    res.status(200).send("Welcome to the about page!");
+app.post("/books", (req, res) => {
+    books.push(req.body);
+    res.status(201).json(new_book);
 });
 
-app.get("/contact", (req, res) => {
-    res.status(200).send("Welcome to the contact page!");
+app.get("/books/:id", (req, res) => {
+    const id = req.params.id;
+    const book = books.find((book) => book.id === parseInt(id));
+    if (book) {
+        res.status(200).json(book);
+    } else {
+        res.status(404).json({ error: "Book not found" });
+    }
 });
+
+app.put("/books/:id", (req, res) => {
+    const id = req.params.id;
+    const index = books.findIndex((book) => book.id === parseInt(id));
+    if (index !== -1) {
+        books[index] = req.body;
+        res.status(200).json(books[index]);
+    } else {
+        books.push(req.body);
+        res.status(201).json(new_book);
+    }
+});
+
+app.patch("/books/:id", (req, res) => {
+    const id = req.params.id;
+    const index = books.findIndex((book) => book.id === parseInt(id));
+    if (index !== -1) {
+        books[index] = { ...books[index], ...req.body };
+        res.status(200).json(books[index]);
+    } else {
+        res.status(404).json({ error: "Book not found" });
+    }
+});
+
+app.delete("/books/:id", (req, res) => {
+    const id = req.params.id;
+    const index = books.findIndex((book) => book.id === parseInt(id));
+    if (index != -1) {
+        books.splice(index, 1);
+        res.status(204).json();
+    } else {
+        res.status(404).json({ error: "Book not found" });
+    }
+})
 
 export default app;
---
+++ src/books.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 1,
+        title: "The Catcher in the Rye",
+    },
+    {
+        id: 2,
+        title: "To Kill a Mockingbird",
+    },
+    {
+        id: 3,
+        title: "1984",
+    },
+    {
+        id: 4,
+        title: "The Grapes of Wrath",
+    },
+    {
+        id: 5,
+        title: "The Great Gatsby",
+    },
+]
